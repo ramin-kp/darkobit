10acb853c83acd394c9719efd480f620a2fd8547
Erfan Safarzad <erfansafarzad7@gmail.com>
2025-07-31 21:52:35 +0330

    fix: responsive subsc-section

--- src/components/templates/home/SubscriptionBanner.tsx
+++ src/components/templates/home/SubscriptionBanner.tsx
@@ -1,66 +1,85 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { Button } from "../../ui/button";
 
 const SubscriptionBanner = () => {
   return (
-    <div className="w-full bg-white py-10 md:px-16">
-      <div className="mx-auto flex flex-col-reverse md:flex-row items-center justify-between gap-10">
+    <section className="relative overflow-x-hidden my-[100px]">
 
-        <div className="flex flex-col items-center md:items-start text-center md:text-right gap-1 md:flex-1">
-          <h2 className="mt-5 text-2xl md:text-2xl font-medium">
+      <div className="">
+        <Image
+          alt="circle"
+          src="/images/circle.png"
+          className="absolute blur-[500px] scale-[70%] -left-[350px] lg:-left-[150px] top-[50px]"
+          height={300}
+          width={500}
+        />
+      </div>
+      
+      <div className="flex flex-col md:flex-row-reverse items-center justify-between px-4">
+
+
+        <div className="z-10 ml-7 flex flex-col">
+          <Image
+            alt="person"
+            src="/images/person-sm.png"
+            className="block lg:hidden"
+            height={300}
+            width={500}
+          />
+          <Image
+            alt="person"
+            src="/images/person-lg.png"
+            className="hidden lg:block"
+            height={300}
+            width={500}
+          />
+          <div className="relative w-[300px] md:w-[400px] h-[70px] mx-auto ">
+            <Image
+              alt="txt-bg"
+              src="/images/txt-bg.png"
+              fill
+              className="object-contain"
+            />
+            <h2 className="absolute inset-0 flex items-center justify-center text-black text-center text-lg text-base-mines px-4">
+              200+ محصول متنوع در دسته بندی
+            </h2>
+          </div>
+        </div>
+
+
+        <div className="flex flex-col w-100 justify-center1 mt-[55px] gap-1">
+          <h2 className="mx-auto font-peyda font-bold text-[25px]">
             دانلود و اشتراک محصولات دیجیتال در
           </h2>
-
-          <div className="mt-0 w-80 h-14 relative">
+          <div className="mx-auto w-80 h-14 relative">
             <Image
               src="/images/logo.svg"
               alt="لوگو دارک بیت"
               fill
-              className=" object-contain"
+              className="object-contain"
             />
           </div>
 
-          <p className="text-gray-600 text-base mt-4 max-sm:px-4 md:text-lg max-w-md">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-            استفاده از طراحان گرافیک است
-          </p>
+          <div className="px-[10px] ">
+            <p className="text-center mt-4  text-base-mines">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+            </p>
+          </div>
 
-          <Button className="bg-secondary-default hover:bg-secondary-default/90 rounded-2xl mt-7 px-6 h-11 flex gap-2 items-center">
-            مشاهده محصولات
-            <svg className="!size-6 shrink-0">
-              <use href="#arrow-left"></use>
-            </svg>
-          </Button>
+            <Button className="bg-secondary-default hover:bg-secondary-default/90 rounded-[18px] mt-7 mx-auto px-6 py-6 h-[44px] w-[170px] flex gap-2 items-center">
+              مشاهده محصولات
+              <svg className="!size-6 shrink-0">
+                <use href="#arrow-left"></use>
+              </svg>
+            </Button>
         </div>
 
-        <div className="relative w-full md:w-[320px]">
-          <div className="relative w-full h-[420px] md:h-96 overflow-hidden">
-            <Image
-              src="/images/Group 1437254053.png"
-              alt="بکگراند"
-              width={600}
-              height={400}
-              className="absolute object-cover scale-[170%] top-[150px] -left-[35%] z-0"
-            />
-          </div>
+        
 
-          <div className="relative px-4 py-4 rounded-3xl text-center overflow-hidden w-fit mx-auto">
-            <Image
-              src="/images/sub-banner-title-bg.png"
-              alt="label background"
-              fill
-              className="scale-x-[110%] object-cover z-0"
-            />
 
-            <div className="relative z-10 text-sm md:text-base font-medium text-gray-800">
-              <span className="font-bold text-3xl text-black"> ۲۰۰+ </span> محصول متنوع در دسته بندی
-            </div>
 
-          </div>
-
-        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
